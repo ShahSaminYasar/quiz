e0205c4c6e68ba7720d1d601a06c722076a9efbb
samin <shahsaminyasarraihan@gmail.com>
2023-04-04 01:59:24 +0600

    Initial Commit

---
+++ script.js
@@ -0,0 +1,278 @@
+var questions = [
+  [
+    {
+      question: "What is the full form of HTML?",
+      options: [
+        "Hyper Typed Multi Language",
+        "Hyper Typed Markup Language",
+        "Hyper Text Markup Language",
+        "Hyper Text Multi Language",
+      ],
+      answer: 2,
+    },
+    {
+      question:
+        "What is the difference between var, let, and const in JavaScript?",
+      options: ["Scope", "Data Type", "Assignment", "All of the above"],
+      answer: 0,
+    },
+    {
+      question: "What is a closure in JavaScript?",
+      options: [
+        "A block of code that can be reused",
+        "A function that runs another function",
+        "A variable that is available only inside a function",
+        "An error that occurs during program execution",
+      ],
+      answer: 1,
+    },
+    {
+      question: "What is the correct way to declare a JavaScript variable?",
+      options: [
+        'var myVariable = "Hello";',
+        'variable myVariable = "Hello";',
+        'myVariable = "Hello";',
+        "None of the above",
+      ],
+      answer: 0,
+    },
+    {
+      question:
+        "Which HTML tag is used to link to an external JavaScript file?",
+      options: ["<link>", "<script>", "<head>", "<body>"],
+      answer: 1,
+    },
+    {
+      question:
+        "Which CSS property is used to set the text color of an element?",
+      options: ["font-size", "background-color", "text-color", "color"],
+      answer: 3,
+    },
+    {
+      question: "Which of the following is not a valid CSS selector?",
+      options: [".myClass", "#myId", "myElement", "*myClass"],
+      answer: 3,
+    },
+    {
+      question: 'What does the "C" in CSS stand for?',
+      options: ["Cascading", "Computer", "Creative", "Code"],
+      answer: 0,
+    },
+    {
+      question: "Which of the following is NOT a valid HTTP status code?",
+      options: ["200", "404", "500", "700"],
+      answer: 3,
+    },
+    {
+      question: "Which of the following is a server-side scripting language?",
+      options: ["JavaScript", "HTML", "CSS", "PHP"],
+      answer: 3,
+    },
+    {
+      question: "What is the purpose of the <meta> tag in HTML?",
+      options: [
+        "To define the document title",
+        "To specify the character encoding",
+        "To link to an external file",
+        "To define the document type",
+      ],
+      answer: 1,
+    },
+  ],
+  [
+    {
+      question: "What is the smallest unit of digital information?",
+      options: ["Byte", "Bit", "Nibble", "Megabyte"],
+      answer: 1,
+    },
+    {
+      question: "What is the full form of CPU?",
+      options: [
+        "Central Processing Unit",
+        "Central Processor Unit",
+        "Computer Processing Unit",
+        "Computer Processor Unit",
+      ],
+      answer: 0,
+    },
+    {
+      question: "What was the first computer virus called?",
+      options: ["Creeper Virus", "Brain", "Elk Cloner", "ILOVEYOU"],
+      answer: 1,
+    },
+    {
+      question: "What does HTML stand for?",
+      options: [
+        "Hyper Text Markup Language",
+        "Hyperlinks and Text Markup Language",
+        "Home Tool Markup Language",
+        "Hyperlink Markup Language",
+      ],
+      answer: 0,
+    },
+    {
+      question: "What does CSS stand for?",
+      options: [
+        "Cascading Style Sheets",
+        "Computer Style Sheets",
+        "Colorful Style Sheets",
+        "Creative Style Sheets",
+      ],
+      answer: 0,
+    },
+    {
+      question:
+        "What is the name of the default browser in Windows operating system?",
+      options: ["Firefox", "Chrome", "Safari", "Microsoft Edge"],
+      answer: 3,
+    },
+    {
+      question: "Who is known as the father of computer?",
+      options: [
+        "Alan Turing",
+        "Charles Babbage",
+        "Tim Berners-Lee",
+        "Bill Gates",
+      ],
+      answer: 1,
+    },
+    {
+      question: "What does RAM stand for?",
+      options: [
+        "Random Access Memory",
+        "Read Access Memory",
+        "Retrieve Acquired Memory",
+        "Recompile Active Memory",
+      ],
+      answer: 0,
+    },
+    {
+      question:
+        "What is the process of a computer receiving information from a server on the internet called?",
+      options: ["Uploading", "Downloading", "Streaming", "Web Browsing"],
+      answer: 1,
+    },
+    {
+      question: "Which planet is known as the Red Planet?",
+      options: ["Venus", "Mars", "Jupiter", "Saturn"],
+      answer: 1,
+    },
+  ],
+];
+
+// DOM
+const questionDisplay = document.getElementById("question");
+const options = document.querySelector(".options");
+const buttons = document.querySelectorAll(".options button");
+const btn0 = document.getElementById("btn0");
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+const scoreDisplay = document.getElementById("score");
+const scoreValue = document.querySelector("#score span");
+const retakeBtn = document.getElementById("retake");
+
+// Select question set
+var choosenQuestionSet = Math.floor(Math.random() * questions.length);
+var questionSet = questions[choosenQuestionSet];
+var score = 0;
+var currentQuestion = 0;
+var question;
+var selectedAnswer;
+var questionTypeAnimation;
+scoreValue.textContent = score + "/" + questionSet.length;
+
+displayQuestion();
+
+function displayQuestion() {
+  if (currentQuestion < questionSet.length) {
+    question = questionSet[currentQuestion];
+
+    if (questionTypeAnimation) {
+      clearInterval(questionTypeAnimation);
+    }
+
+    let index = 0;
+    questionTypeAnimation = setInterval(() => {
+      if (index < question.question.length) {
+        questionDisplay.textContent += question.question.charAt(index);
+        index++;
+      } else {
+        options.classList.add("visible");
+        clearInterval(questionTypeAnimation);
+      }
+    }, 18);
+
+    btn0.textContent = question.options[0];
+    btn1.textContent = question.options[1];
+    btn2.textContent = question.options[2];
+    btn3.textContent = question.options[3];
+  } else {
+    retakeScreen();
+  }
+}
+
+btn0.addEventListener("click", () => {
+  checkAnswer(0);
+});
+
+btn1.addEventListener("click", () => {
+  checkAnswer(1);
+});
+
+btn2.addEventListener("click", () => {
+  checkAnswer(2);
+});
+
+btn3.addEventListener("click", () => {
+  checkAnswer(3);
+});
+
+function checkAnswer(selectedAnswer) {
+  options.classList.add("disable");
+  if (selectedAnswer == question.answer) {
+    score++;
+    scoreValue.textContent = score + "/" + questionSet.length;
+    document.getElementById(`btn${selectedAnswer}`).classList.add("correct");
+    currentQuestion++;
+    setTimeout(() => {
+      nextQuestion();
+    }, 1000);
+  } else {
+    currentQuestion++;
+    document.getElementById(`btn${selectedAnswer}`).classList.add("incorrect");
+    document.getElementById(`btn${question.answer}`).classList.add("correct");
+    setTimeout(() => {
+      nextQuestion();
+    }, 1000);
+  }
+}
+
+function nextQuestion() {
+  options.classList.remove("disable");
+  options.classList.remove("visible");
+  buttons.forEach((button) => {
+    button.classList.remove("correct", "incorrect");
+  });
+  setTimeout(() => {
+    questionDisplay.textContent = "";
+    displayQuestion();
+  }, 500);
+}
+
+function retakeScreen() {
+  retake.classList.add("active");
+  scoreDisplay.classList.add("active");
+}
+
+retakeBtn.addEventListener("click", () => {
+  retake.classList.remove("active");
+  scoreDisplay.classList.remove("active");
+  score = 0;
+  choosenQuestionSet = Math.floor(Math.random() * questions.length);
+  console.log(choosenQuestionSet);
+  questionSet = questions[choosenQuestionSet];
+  scoreValue.textContent = score + "/" + questionSet.length;
+  currentQuestion = 0;
+  displayQuestion();
+});
